50e4d2ce1c066cf274409a4e1bd87fd551e5d065
Glauco Todesco <glauco.todesco@gmail.com>
2021-11-01 17:19:37 -0300

    Finished Crud Users

--- src/main/java/com/wsmongo/springbootmongo/resources/UserResource.java
+++ src/main/java/com/wsmongo/springbootmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.wsmongo.springbootmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import com.wsmongo.springbootmongo.models.dtos.UserDTO;
@@ -7,9 +8,15 @@ import com.wsmongo.springbootmongo.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/users")
@@ -19,7 +26,33 @@ public class UserResource {
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<UserDTO>> getAllUsers(){
+    public ResponseEntity<List<UserDTO>> getAllUsers() {
         return ResponseEntity.ok(userService.findAll());
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        UserDTO obj = userService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> insert(@RequestBody UserDTO obj) {
+        obj = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> update(@PathVariable String id, @RequestBody UserDTO obj) {
+        obj = userService.update(id, obj);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/com/wsmongo/springbootmongo/services/UserService.java
+++ src/main/java/com/wsmongo/springbootmongo/services/UserService.java
@@ -7,7 +7,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import com.wsmongo.springbootmongo.models.dtos.UserDTO;
+import com.wsmongo.springbootmongo.models.entities.User;
 import com.wsmongo.springbootmongo.repositories.UserRepository;
+import com.wsmongo.springbootmongo.resources.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,4 +24,38 @@ public class UserService {
 		
 	}
     
+    public UserDTO findById(String id) {
+        User user = getEntityById(id);
+        return new UserDTO(user);
+    }
+
+    public UserDTO insert(UserDTO dto) {
+		User entity = new User();
+		copyDtoToEntity(dto, entity);
+		entity = repository.insert(entity);
+		return new UserDTO(entity);
+	}
+
+    public UserDTO update(String id, UserDTO dto) {
+		User entity = getEntityById(id);
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new UserDTO(entity);
+	}
+
+    private User getEntityById(String id) {
+        return repository.findById(id)
+                         .orElseThrow( () -> new ResourceNotFoundException("Objeto não encontrado"));
+    }
+
+    private void copyDtoToEntity(UserDTO dto, User entity) {
+		entity.setName(dto.getName());
+		entity.setEmail(dto.getEmail());
+	}
+
+    public void delete(String id) {
+		getEntityById(id);
+		repository.deleteById(id);
+	}
+
 }
